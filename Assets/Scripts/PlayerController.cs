994637d9650cef14840ef2e0cf713c8b92fc31d9
RodrigoSilva_a59659 <a59659@alunos.ipb.pt>
2025-03-16 20:30:35 +0000

    Implement a cooldown after the character takes damage

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -15,8 +15,14 @@ public class PlayerController : MonoBehaviour
 
   // Variables related to the health system
   public int maxHealth = 5;
-  public int health { get { return currentHealth; }}
   int currentHealth;
+  public int health { get { return currentHealth; }}
+
+
+  // Variables related to temporary invincibility
+  public float timeInvincible = 2.0f;
+  bool isInvincible;
+  float damageCooldown;
 
 
   // Start is called before the first frame update
@@ -24,6 +30,8 @@ public class PlayerController : MonoBehaviour
   {
      MoveAction.Enable();
      rigidbody2d = GetComponent<Rigidbody2D>();
+
+
      currentHealth = maxHealth;
   }
  
@@ -31,10 +39,20 @@ public class PlayerController : MonoBehaviour
   void Update()
   {
      move = MoveAction.ReadValue<Vector2>();
-  }
 
 
-  // FixedUpdate has the same call rate as the physics system
+     if (isInvincible)
+     {
+         damageCooldown -= Time.deltaTime;
+         if (damageCooldown < 0)
+         {
+            isInvincible = false;
+         }
+     }
+   }
+
+
+// FixedUpdate has the same call rate as the physics system
   void FixedUpdate()
   {
      Vector2 position = (Vector2)rigidbody2d.position + move * speed * Time.deltaTime;
@@ -44,8 +62,20 @@ public class PlayerController : MonoBehaviour
 
   public void ChangeHealth (int amount)
   {
+     if (amount < 0)
+     {
+        if (isInvincible)
+        {
+           return;
+        }
+     isInvincible = true;
+     damageCooldown = timeInvincible;
+  }
+
+
      currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
      Debug.Log(currentHealth + "/" + maxHealth);
   }
+
 
 }
