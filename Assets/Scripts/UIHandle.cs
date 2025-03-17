4da0ba156edb89d18548c678f1775b705ec16806
RodrigoSilva_a59659 <a59659@alunos.ipb.pt>
2025-03-17 22:08:50 +0000

    Create the dialogue UI

--- Assets/Scripts/UIHandle.cs
+++ Assets/Scripts/UIHandle.cs
@@ -3,8 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UIElements;
 public class UIHandle : MonoBehaviour
-{ private VisualElement m_Healthbar;
+{ 
+     private VisualElement m_Healthbar;
    public static UIHandle instance { get; private set; }
+
+   // UI dialogue window variables
+   public float displayTime = 4.0f;
+   private VisualElement m_NonPlayerDialogue;
+   private float m_TimerDisplay;
 
 
    // Awake is called when the script instance is being loaded (in this situation, when the game scene loads)
@@ -13,26 +19,47 @@ public class UIHandle : MonoBehaviour
        instance = this;
    }
 
-
-
-
    // Start is called before the first frame update
-   void Start()
+   private void Start()
    {
        UIDocument uiDocument = GetComponent<UIDocument>();
        m_Healthbar = uiDocument.rootVisualElement.Q<VisualElement>("HealthBar");
        SetHealthValue(1.0f);
+
+
+       m_NonPlayerDialogue = uiDocument.rootVisualElement.Q<VisualElement>("NPCDialogue");
+       m_NonPlayerDialogue.style.display = DisplayStyle.None;
+       m_TimerDisplay = -1.0f;
+
+
    }
 
 
+
+   private void Update()
+   {
+       if (m_TimerDisplay > 0)
+       {
+           m_TimerDisplay -= Time.deltaTime;
+           if (m_TimerDisplay < 0)
+           {
+               m_NonPlayerDialogue.style.display = DisplayStyle.None;
+           }
+
+
+       }
+   }
 
 
    public void SetHealthValue(float percentage)
    {
        m_Healthbar.style.width = Length.Percent(100 * percentage);
-
-
    }
-
    
+   
+   public void DisplayDialogue()
+   {
+    m_NonPlayerDialogue.style.display = DisplayStyle.Flex;
+    m_TimerDisplay = displayTime;
+   }
 }
