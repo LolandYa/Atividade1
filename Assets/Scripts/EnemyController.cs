d44ea1b662bfd5736cc745a0f6f6381a9be74bcf
RodrigoSilva_a59659 <a59659@alunos.ipb.pt>
2025-03-17 15:56:42 +0000

    Write a script to move the enemy

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+   public float speed;
+   Rigidbody2D rigidbody2d;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
+   // Start is called before the first frame update
+   void Start()
+   {
+       rigidbody2d = GetComponent<Rigidbody2D>();
+   }
+
+
+  // FixedUpdate has the same call rate as the physics system
+  void FixedUpdate()
+  {
+     Vector2 position = rigidbody2d.position;
+     position.x = position.x + speed * Time.deltaTime;
+     rigidbody2d.MovePosition(position);
+  }
 }
